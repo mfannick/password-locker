9ace2b2b584c8b4317874109d6f7dc7210fd6b5a
mfannick <mfannick1@gmail.com>
2019-09-05 19:05:18 +0200

    creating  search user function test

--- user_test.py
+++ user_test.py
@@ -45,33 +45,33 @@ class TestClasses(unittest.TestCase):
         self.assertEqual(User.displayUser(),User.userList)
 
 # ##############################################################
-# # setup and class creation up here
-#     def tearDown(self):
-#             '''
-#             tearDown method that cleans up after each test case has run.
-#             '''
-#             User.userList = []
-# #saving multiple times
-#     def testSaveMultiUser(self):
-#         '''
-#         testSaveMultiUser test checks if we can save multiple user
-#         objects to our userList
-#         ''' 
-#         self.newUser.saveUser()
-#         testUser= User("Annick1","Francine1","mfannick1","anny121")
-#         testUser.saveUser()
-#         self.assertEqual(len(User.userList),2) 
+# setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that cleans up after each test case has run.
+            '''
+            User.userList = []
+#saving multiple times
+    def testSaveMultiUser(self):
+        '''
+        testSaveMultiUser test checks if we can save multiple user
+        objects to our userList
+        ''' 
+        self.newUser.saveUser()
+        testUser= User("Annick1","Francine1","mfannick1","anny121")
+        testUser.saveUser()
+        self.assertEqual(len(User.userList),2) 
 
         
 
 # ##############################################################
-# #searching user identity  
-#     def testFindUserByUserName(self):
-#         self.newUser.saveUser()
-#         testUser=User("Annick1","Francine1","mfannick1","anny121")
-#         testUser.saveUser()
-#         found_user= User.findByUserName("mfannick1")
-#         self.assertEqual(found_user.fname,testUser.fname)
+#searching user identity  
+    def testFindUserByUserName(self):
+        self.newUser.saveUser()
+        testUser=User("Annick1","Francine1","mfannick1","anny121")
+        testUser.saveUser()
+        found_user= User.findByUserName("mfannick1")
+        self.assertEqual(found_user.fname,testUser.fname)
        
 if __name__ == '__main__':
     unittest.main() 
